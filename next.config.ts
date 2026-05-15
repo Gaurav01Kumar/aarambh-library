42bdd8ec5aecefcf87cbfe62e763824c1678c138
Gaurav Kumar <gk1427473@gmail.com>
2026-05-15 12:01:14 +0530

    fix :ui

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "www.library.yaadgarpal.com",
+    "library.yaadgarpal.com",
+    "localhost",
+    "[IP_ADDRESS]",
+  ],
 };
 
 export default nextConfig;
